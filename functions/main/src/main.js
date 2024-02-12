4334be03e8b82dbf41202412e7a0ff7a0775c664
AapicoDev <app.aapico@gmail.com>
2024-02-12 15:20:00 +0700

    test fetch assets

--- functions/main/src/main.js
+++ functions/main/src/main.js
@@ -1,15 +1,39 @@
+import { Client, Databases } from 'node-appwrite';
 
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
   // Why not try the Appwrite SDK?
   //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+  const client = new Client()
+    .setEndpoint('https://baas.powermap.live/v1')
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+  const databases = new Databases(client);
 
+  try {
+    let promise = await databases.listDocuments(
+      '659eacd792b60f142082',
+      '659eaceec0cffe8ed41b',
+    )
+
+    promise.then(function (response) {
+      log(response);
+    }, function (error) {
+      error(error);
+    });
+
+  } catch (e) {
+    error("Failed to fetch assets: " + e.message)
+    return res.send("Failed to fetch asset")
+  }
+
+
+
+
+  // `res.json()` is a handy helper for sending JSON
+  return res.json({
+    status: '200',
+  });
 };
